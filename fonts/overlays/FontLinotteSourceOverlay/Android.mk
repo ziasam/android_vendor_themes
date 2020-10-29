1bf712af2f1e2ba5c151c47ca83a447d2ddd92bb
spezi77 <spezi7713@gmx.net>
2020-10-29 11:31:27 +0100

    fonts: fixup Linotte font.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- fonts/overlays/FontLinotteSourceOverlay/Android.mk
+++ fonts/overlays/FontLinotteSourceOverlay/Android.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := FontLinotteSource
+LOCAL_RRO_THEME := FontLinotteSourceOverlay
 LOCAL_CERTIFICATE := platform
 LOCAL_PRODUCT_MODULE := true
 
